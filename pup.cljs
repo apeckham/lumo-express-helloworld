db08e66593e635d2f2b7168f6129e00e85733f75
apeckham <apeckham@gmail.com>
2017-11-07 21:44:05 -0800

    simpler example

--- pup.cljs
+++ pup.cljs
@@ -1,14 +1,12 @@
 (.setFlagsFromString (js/require "v8") "--no-use_strict")
 
 (def puppeteer (js/require "puppeteer"))
-(def Q (js/require "q"))
 
-(-> (Q (.launch puppeteer))
+(-> (.launch puppeteer)
     (.then (fn [browser]
-             (-> (Q (.newPage browser))
+             (-> (.newPage browser)
                  (.then (fn [page]
                           (.goto page "https://example.com/")
                           (.screenshot page #js {:path "example.png"})))
-                 (.then #(.close browser))))))
-
-#_(-> (.launch puppeteer) (.then #(.newPage %)) (.then #(do (.goto % "http://example.com")) (.screenshot % #js {:path "example.png"})))
+                 (.then #(.close browser)))))
+    (.then #(js/console.log "done!")))
