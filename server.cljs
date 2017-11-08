749f38c8683742cd0d35f46add9a64592cd0d40c
apeckham <apeckham@gmail.com>
2017-11-07 23:03:22 -0800

    write to a temporary file!

--- server.cljs
+++ server.cljs
@@ -10,20 +10,22 @@
 (.setFlagsFromString (js/require "v8") "--no-use_strict")
 (defonce puppeteer (js/require "puppeteer"))
 (defonce file-url (js/require "file-url"))
+(defonce tmp (js/require "tmp"))
 
 (. app (get "/hello"
   (fn [req res]
-    (-> (.launch puppeteer)
-        (.then (fn [browser]
-                 (-> (.newPage browser)
-                     (.then (fn [page]
-                              (.setViewport page #js {:width 2000 :height 2000})
-                              (-> (.goto page (file-url "page.html") #js {:waitUntil "networkidle0"})
-                                  (.then #(.screenshot page #js {:path "example.png"})))))
-                     (.then #(.close browser))
-                     (.catch #(js/console.log %)))))
-        (.then #(.sendFile res "example.png" #js {:root js/__dirname}))
-        (.catch #(js/console.log %))))))
+    (.file tmp #js {:postfix ".png"} (fn [err tmp-path]
+                 (-> (.launch puppeteer)
+                     (.then (fn [browser]
+                              (-> (.newPage browser)
+                                  (.then (fn [page]
+                                           (.setViewport page #js {:width 2000 :height 2000})
+                                           (-> (.goto page (file-url "page.html") #js {:waitUntil "networkidle0"})
+                                               (.then #(.screenshot page #js {:path tmp-path})))))
+                                  (.then #(.close browser))
+                                  (.catch #(js/console.log %)))))
+                     (.then #(.sendFile res tmp-path))
+                     (.catch #(js/console.log %))))))))
 
 (doto (.createServer http #(app %1 %2))
   (.listen 3000))
