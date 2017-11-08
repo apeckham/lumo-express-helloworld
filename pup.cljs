b5c0a94a853659ec4ad755cc12e0a8fc6035ff46
apeckham <apeckham@gmail.com>
2017-11-07 20:26:22 -0800

    pup hw

---
+++ pup.cljs
@@ -0,0 +1,12 @@
+(.setFlagsFromString (js/require "v8") "--no-use_strict")
+
+(def puppeteer (js/require "puppeteer"))
+(def Q (js/require "q"))
+
+(-> (Q (.launch puppeteer))
+    (.then (fn [browser]
+             (-> (Q (.newPage browser))
+                 (.then (fn [page]
+                          (.goto page "https://example.com/")
+                          (.screenshot page #js {:path "example.png"})))
+                 (.then #(.close browser))))))
